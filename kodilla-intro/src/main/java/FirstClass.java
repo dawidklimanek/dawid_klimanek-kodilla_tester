2b0ea5381661499c3f0d0b5f86b9bdcd6c4bb81e
Dawid Klimanek <dawid.klimanek@gmail.com>
2022-07-31 22:15:09 +0200

    Dla mentora

---
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -0,0 +1,6 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        String Firstclass = "Hello from FirstClass!";
+        System.out.println(Firstclass);
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String fathersName = "Jerzy Klimanek";
+                int fahtesage = 66;
+                        double ilewazy = 84.8;
+                                char miseczka = 'D';
+                                        boolean niejestdobry = false;
+                                                int ilepalcow = 10;
+        System.out.println(fathersName);
+        System.out.println(fahtesage);
+        System.out.println (ilewazy);
+        System.out.println (miseczka);
+        System.out.println (niejestdobry);
+        System.out.println  (ilepalcow);
+    }
+}
