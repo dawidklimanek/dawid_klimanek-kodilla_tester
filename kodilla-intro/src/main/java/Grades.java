386feb8e28110d3cb74e7fa0168aa922fa17c088
Dawid Klimanek <dawid.klimanek@gmail.com>
2022-08-12 20:10:17 +0200

    Dla mentora 12.08

--- kodilla-intro/src/main/java/Grades.java
+++ kodilla-intro/src/main/java/Grades.java
@@ -1,4 +1,25 @@
 public class Grades {
-    private int [] grades;
-    public Grades
+    private int[] grades;
+    private int size;
+
+    public Grades() {
+        this.grades = new int[10];
+        this.size = 0;
+    }
+
+    public void add(int value) {
+        if (this.size == 10) {
+            return;
+        }
+        this.grades[this.size] = value;
+        this.size++;
+    }
+    double srednia(int a, int b, int c) {
+        double result = (a + b) / c;
+        return result;
+    }
+    public int[] getGrades() {
+        return grades;
+    }
 }
+
