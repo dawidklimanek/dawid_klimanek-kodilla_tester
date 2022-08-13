3f1ae1e7f3426e6a526da0e434d6908997f20e05
Dawid Klimanek <dawid.klimanek@gmail.com>
2022-08-13 23:44:59 +0200

    nowy

--- kodilla-intro/src/main/java/User.java
+++ kodilla-intro/src/main/java/User.java
@@ -2,6 +2,11 @@ public class User {
             String x;
             int y;
                     User (String x, int y){
-
+                        this.x= x;
+                        this.y=y;
                     }
+    int result = 0;
+for (int i = 0; i < numbers.length; i++) {
+        result = result + numbers[i];
+    }
 }
