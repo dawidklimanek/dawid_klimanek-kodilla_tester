06285c53483f70f51bec88260812587eec853e91
Dawid Klimanek <dawid.klimanek@gmail.com>
2022-09-04 16:33:09 +0200

    Merge remote-tracking branch 'origin/master1' into master1

--- kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java
@@ -1,11 +1,26 @@
 package com.kodilla.bank.homework;
 
 public class Bank {
+    private int[] bank;
+    private int size;
     private CashMachine[] cashMachines;
 
     public Bank(CashMachine[] cashMachines) {
-        this.cashMachines = cashMachines;
 
+    }
+
+    public Bank() {
+        this.bank = new int[0];
+        this.size = 0;
+
+    }
+
+    public void add(int cashMachine) {// metody, która umożliwi dodanie elementów do tablicy z bankomatami
+        this.size++;
+        int[] newTab = new int[this.size];
+        System.arraycopy(bank, 0, newTab, 0, bank.length);
+        newTab[this.size - 1] = cashMachine;
+        this.bank = newTab;
     }
 
     public int avgOfNegativeTransactions() {
@@ -23,36 +38,44 @@ public class Bank {
         return (avgOfNegative / numberNegative);
     }
 
-   public int avgOfPositiveTransaction() {
-       int avgOfPositive = 0;
-       int numberPositive = 0;
-       for (CashMachine cashMachine : cashMachines) {
-           avgOfPositive = avgOfPositive + cashMachine.sumOfPositiveTransactions();
-           numberPositive = numberPositive + cashMachine.numberOfNegativeTransactions();
+    public int avgOfPositiveTransaction() {
+        int avgOfPositive = 0;
+        int numberPositive = 0;
+        for (CashMachine cashMachine : cashMachines) {
+            avgOfPositive = avgOfPositive + cashMachine.sumOfPositiveTransactions();
+            numberPositive = numberPositive + cashMachine.numberOfNegativeTransactions();
 
-       }
-       if (numberPositive == 0) {
-           return avgOfPositive;
-       }
+        }
+        if (numberPositive == 0) {
+            return avgOfPositive;
+        }
 
-       return (avgOfPositive / numberPositive);
-   }
+        return (avgOfPositive / numberPositive);
+    }
 
-    public int sumOfNegativeTransaction() {
+    public int sumOfNegativeTransaction() {//- metody liczącej liczbę transakcji związanych z wypłatą
         int sumNegative = 0;
         for (CashMachine cashMachine : cashMachines) {
             sumNegative = sumNegative + cashMachine.sumOfNegativeTransaction();
         }
         return sumNegative;
     }
-
-    public int balance() {
-        int balance = 0;
+    public int sumOfPositiveTransaction() {//- metoda licząca liczbę transakcji związaną z wpłatą pieniędzy
+        int sumPositive = 0;
         for (CashMachine cashMachine : cashMachines) {
-            balance = balance + cashMachine.balance();
+            sumPositive = sumPositive + cashMachine.sumOfNegativeTransaction();
+        }
+        return sumPositive;
+
+}
+
+    public int bilans() {// metody liczącej bilans banku
+        int bilans = 0;
+        for (CashMachine cashMachine : cashMachines) {
+            bilans = bilans + cashMachine.balance();
 
         }
-        return balance;
+        return bilans;
     }
 }
 
