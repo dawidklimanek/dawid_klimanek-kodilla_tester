8a273e8e379bc4d056c4ced6d7ef92615ff6435f
Dawid Klimanek <dawid.klimanek@gmail.com>
2022-09-15 08:15:24 +0200

    Merge remote-tracking branch 'origin/master1' into master1
    # Conflicts:
    #	kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
@@ -8,16 +8,9 @@ public class BookApplication {
         BookManager.createBook ("Mała","DAvid");
         BookManager.createBook("Wojna","Borys");
         System.out.println("-----");
-        System.out.println("Directory size: " + BookManager.bookDirectory.size());
+        System.out.println("Directory size: " + BookManager.getSize());
 
-        System.out.println("--------------");
-        System.out.println("Do objects have the same hashCodes?");
-        System.out.println("0 and 1");
-        System.out.println(BookManager.bookDirectory.get(0).equals(BookManager.bookDirectory.get(1)));
-        System.out.println("1 and 2");
-        System.out.println(BookManager.bookDirectory.get(1).equals(BookManager.bookDirectory.get(2)));
-        System.out.println("0 and 2");
-        System.out.println(BookManager.bookDirectory.get(0).equals(BookManager.bookDirectory.get(2)));
+
     }
 
 }
--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
@@ -4,16 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BookManager {
-    public static List<Book> bookDirectory = new ArrayList<>();
+    private static List<Book> bookDirectory = new ArrayList<>();
 
 
     public static Book createBook(String title, String author) {
         Book book = new Book(title, author);
-        if (bookDirectory.contains(book) == false) {
+        if (!bookDirectory.contains(book)) {
             System.out.println("Book isn't in directory  ");
-            bookDirectory.add(book);
-        } else {
-            System.out.println("Book is in directory  ");
             bookDirectory.add(book);
 
         }
@@ -21,12 +18,11 @@ public class BookManager {
         System.out.println(new Book(title, author).hashCode());
         return book;
     }
-
-    public void getIndex() {
-        for (int i = 0; i < BookManager.bookDirectory.size(); i++) {
-            bookDirectory.get(i);
-        }
+    public static int getSize (){
+        return bookDirectory.size();
     }
 }
+
+
 
 
