7ed3a4841b6d25fecf818a850a1210145081745e
Dawid Klimanek <dawid.klimanek@gmail.com>
2022-09-27 22:31:11 +0200

    Merge remote-tracking branch 'origin/master1' into master1
    # Conflicts:
    #	kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java

---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
@@ -0,0 +1,54 @@
+package com.kodilla.parametrized_tests.homework;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+class GamblingMachineTestSuite {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/GoodNumbers.csv")
+    public void shouldCheckWinsWorks(int value)
+            throws InvalidNumbersException {
+        GamblingMachine gamblingMachine = new GamblingMachine();
+        Set<Integer> set = new HashSet<>();
+        set.add(1);
+
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/WrongNumbers.csv")
+    public void shouldErrorIfAddOneGoodValue(int value) {
+        GamblingMachine gamblingMachine = new GamblingMachine();
+        Set<Integer> set = new HashSet<>();
+        set.add(1);
+
+        Assertions.assertThrows(InvalidNumbersException.class, () -> {
+            gamblingMachine.howManyWins(set);
+        });
+
+    }
+
+    @Test
+    public void shouldErrorIfAddOneGoodValue() {
+        GamblingMachine gamblingMachine = new GamblingMachine();
+        Set<Integer> set = new HashSet<>();
+        set.add(5);
+        Assertions.assertThrows(InvalidNumbersException.class, () -> {
+            gamblingMachine.howManyWins(set);
+        });
+    }
+
+    @Test
+    public void shouldComputerAddGoodValues()  {
+        GamblingMachine gamblingMachine = new GamblingMachine();
+        Set<Integer> set = gamblingMachine.getGenerateComputerNumbers();
+        Assertions.assertEquals(6, set.size());
+        Assertions.assertTrue(Collections.max(set) <= 50);
+        Assertions.assertTrue(Collections.min(set) > 0);
+    }
+}
