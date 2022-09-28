07b3854992e30f8332b86d25d34d9dfef92ce337
Dawid Klimanek <dawid.klimanek@gmail.com>
2022-09-28 15:46:04 +0200

    Merge remote-tracking branch 'origin/master1' into master1
    # Conflicts:
    #	kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
@@ -1,54 +1,47 @@
 package com.kodilla.parametrized_tests.homework;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 class GamblingMachineTestSuite {
+    private Set<Integer> convert(String input) {
+        String[] strings = input.split(",");
+        Set<Integer> numbers = new HashSet<>();
+        for (String number : strings) {
+            numbers.add(Integer.parseInt(number));
+
+        }
+        return numbers;
+
+    }
+
     @ParameterizedTest
-    @CsvFileSource(resources = "/GoodNumbers.csv")
-    public void shouldCheckWinsWorks(int value)
+    @CsvFileSource(resources = "/goodNumbers.csv")
+
+    public void shouldCheckWinsWorks(String value)
             throws InvalidNumbersException {
+        Set<Integer> numbers = convert(value);
         GamblingMachine gamblingMachine = new GamblingMachine();
-        Set<Integer> set = new HashSet<>();
-        set.add(1);
+        int result = gamblingMachine.howManyWins(numbers);
+        Assertions.assertTrue(result >= 0 & result <= 6);
+
 
     }
 
     @ParameterizedTest
-    @CsvFileSource(resources = "/WrongNumbers.csv")
-    public void shouldErrorIfAddOneGoodValue(int value) {
+    @CsvFileSource(resources = "/wrongNumbers.csv")
+    public void shouldErrorIfAddOneGoodValue(String value) {
+        Set<Integer> numbers = convert(value);
         GamblingMachine gamblingMachine = new GamblingMachine();
-        Set<Integer> set = new HashSet<>();
-        set.add(1);
+
 
         Assertions.assertThrows(InvalidNumbersException.class, () -> {
-            gamblingMachine.howManyWins(set);
+            gamblingMachine.howManyWins(numbers);
         });
-
-    }
-
-    @Test
-    public void shouldErrorIfAddOneGoodValue() {
-        GamblingMachine gamblingMachine = new GamblingMachine();
-        Set<Integer> set = new HashSet<>();
-        set.add(5);
-        Assertions.assertThrows(InvalidNumbersException.class, () -> {
-            gamblingMachine.howManyWins(set);
-        });
-    }
-
-    @Test
-    public void shouldComputerAddGoodValues()  {
-        GamblingMachine gamblingMachine = new GamblingMachine();
-        Set<Integer> set = gamblingMachine.getGenerateComputerNumbers();
-        Assertions.assertEquals(6, set.size());
-        Assertions.assertTrue(Collections.max(set) <= 50);
-        Assertions.assertTrue(Collections.min(set) > 0);
     }
 }
+
