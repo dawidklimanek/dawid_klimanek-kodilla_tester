47eba41c263dbc5c6a58d790a750e31c064de2f5
Dawid Klimanek <dawid.klimanek@gmail.com>
2022-09-29 21:32:44 +0200

    Merge remote-tracking branch 'origin/master1' into master1
    # Conflicts:
    #	kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java

---
+++ kodilla-advanced-tests/src/main/java/com/kodilla/parametrized_tests/StringSources.java
@@ -0,0 +1,14 @@
+package com.kodilla.parametrized_tests;
+
+public class StringSources {
+
+//    static Stream<Arguments> provideStringsForTestingLength() {
+//        return Stream.of(
+//                Arguments.of("test", 4),
+//                Arguments.of("OtHEr ", 5),
+//                Arguments.of("E V e n t", 5),
+//                Arguments.of("null ", 4),
+//                Arguments.of("A", 1)
+//        );
+//    }
+}
---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
@@ -0,0 +1,39 @@
+package com.kodilla.parametrized_tests.homework;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import sun.tools.jstat.Arguments;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class PersonTestsuite {
+    class PersonTestSuite {
+
+        static Stream<Arguments> provideDataForTestingBMI() {
+            return Stream.of(
+                    Arguments.of(new Person(1.76,85)),
+                    Arguments.of(new Person(1.54,120)),
+                    Arguments.of(new Person(1.65,57)),
+                    Arguments.of(new Person(1.57,52)),
+                    Arguments.of(new Person(1.93,48)),
+                    Arguments.of(new Person(1.78,150))
+            );
+        }
+
+        @ParameterizedTest
+        @MethodSource(value = "com.kodilla.parametrized_tests.homework.DataSources#provideDataForTestingBMI")
+        void shouldCalculateBMIWithProvidedDataExternalSource(Person person) {
+            System.out.println(person.getBMI());
+            assertNotNull(person.getBMI());
+        }
+
+        @ParameterizedTest
+        @MethodSource(value = "provideDataForTestingBMI")
+        void shouldCalculateBMIWithProvidedDataInternalSource(Person person) {
+            System.out.println(person.getBMI());
+            assertNotNull(person.getBMI());
+        }
+    }
+}
