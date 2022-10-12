86acd72ad87712f7b4c427701067e155b706d293
Dawid Klimanek <dawid.klimanek@gmail.com>
2022-10-12 19:17:32 +0200

    Merge remote-tracking branch 'origin/master1' into master1
    # Conflicts:
    #	kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java

---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependence_injection/homework/Calculator.java
@@ -0,0 +1,37 @@
+package com.kodilla.spring.basic.spring_dependence_injection.homework;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Calculator {
+    Display display;
+
+    public Calculator(Display display) {
+        this.display = display;
+    }
+
+    public double add(double a, double b) {
+        double result = a + b;
+        return display.display(result);
+    }
+
+    public double subtract(double a, double b) {
+        double result = a - b;
+        return display.display(result);
+    }
+
+    public double multiply(double a, double b) {
+        double result = a * b;
+        return display.display(result);
+    }
+
+    public double divide(double a, double b) {
+        double result = a / b;
+        if (b != 0) {
+            return display.display(result);
+        } else {
+            System.out.print("Don't divide by: ");
+            return 0.0;
+        }
+    }
+}
