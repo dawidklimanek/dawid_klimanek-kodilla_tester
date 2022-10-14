56caf6733c9e87e4fd4c41f1039f848ff72f1dd9
Dawid Klimanek <dawid.klimanek@gmail.com>
2022-10-14 08:58:51 +0200

    Merge remote-tracking branch 'origin/master1' into master1
    # Conflicts:
    #	kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
@@ -13,12 +13,12 @@ class PersonTestsuite {
 
     static Stream<Arguments> provideDataForTestingBMI() {
         return Stream.of(
-                Arguments.of(new Person(1.76, 85)),
-                Arguments.of(new Person(1.54, 120)),
-                Arguments.of(new Person(1.65, 57)),
-                Arguments.of(new Person(1.57, 52), ""),
-        Arguments.of(new Person(1.93, 48), ""),
-                Arguments.of(new Person(1.78, 150), "")
+                Arguments.of(new Person(1.76, 85),"Overweight"),
+                Arguments.of(new Person(1.54, 120),"Obese Class V (Super Obese)"),
+                Arguments.of(new Person(1.65, 57),"Normal (healthy weight)"),
+                Arguments.of(new Person(1.57, 52), "Normal (healthy weight)"),
+                Arguments.of(new Person(1.93, 48), "Very severely underweight"),
+                Arguments.of(new Person(1.78, 150), "Obese Class IV (Morbidly Obese)")
             );
 
     }
---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependence_injection/MessageService.java
@@ -0,0 +1,6 @@
+package com.kodilla.spring.basic.spring_dependence_injection;
+
+    public interface MessageService {
+
+        String send(String message, String receiver);
+}
