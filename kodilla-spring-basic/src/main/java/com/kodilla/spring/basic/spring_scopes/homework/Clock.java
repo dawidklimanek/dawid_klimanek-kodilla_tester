0583596404aff98284a0de299e3d7f4baba3a0bc
Dawid Klimanek <dawid.klimanek@gmail.com>
2022-10-16 21:50:38 +0200

    Merge remote-tracking branch 'origin/master1' into master1
    # Conflicts:
    #	kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java

---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_scopes/homework/Clock.java
@@ -0,0 +1,40 @@
+package com.kodilla.spring.basic.spring_scopes.homework;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
+import java.util.Objects;
+
+@Component
+@Scope("prototype")
+public class Clock {
+
+    LocalTime time;
+
+    public Clock() {
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        this.time = LocalTime.now();
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Clock clock = (Clock) o;
+        return Objects.equals(time, clock.time);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(time);
+    }
+}
---
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_scopes/homework/ClockTestSuite.java
@@ -0,0 +1,29 @@
+package com.kodilla.spring.basic.spring_scopes.homework;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.RepeatedTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.time.LocalTime;
+
+@SpringBootTest
+public class ClockTestSuite{
+
+    @RepeatedTest(5)
+    public void shouldReturnCurrentTime() {
+
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Clock time1 = context.getBean(Clock.class);
+        Clock time2 = context.getBean(Clock.class);
+        Clock time3 = context.getBean(Clock.class);
+        LocalTime one = time1.getTime();
+        LocalTime two = time2.getTime();
+        LocalTime three = time3.getTime();
+
+        Assertions.assertNotEquals(one, two);
+        Assertions.assertNotEquals(two, three);
+        Assertions.assertNotEquals(one, three);
+    }
+}
